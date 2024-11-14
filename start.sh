60fc3fa51288802e12fa63423dcb9c4aac667e1c
acallaghan-ch <acallaghan@companieshouse.gov.uk>
2024-11-14 11:13:58 +0000

    SAN-49: Revert env variable name

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ if [[ -z "${MESOS_SLAVE_PID}" ]]; then
     source ~/.chs_env/global_env
     source ~/.chs_env/lfp-pay-api/env
 
-    PORT="${PPS_PAY_API_PORT}"
+    PORT="${LFP_PAY_API_PORT}"
 else
     PORT="$1"
     CONFIG_URL="$2"
