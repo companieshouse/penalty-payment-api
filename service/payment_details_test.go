3e49bbe8e4f75937c468fb386b373c175401be9f
klai-ch <klai@companieshouse.gov.uk>
2025-02-24 11:14:13 +0000

    SAN-303 Update from review

--- service/payment_details_test.go
+++ service/payment_details_test.go
@@ -61,305 +61,193 @@ func TestUnitGetPaymentDetailsFromPayableResource(t *testing.T) {
 
 	Convey("Get payment details pending state - success", t, func() {
 
-		path := "/company/12345678/penalties/late-filing/abcdef/payment"
-		req := httptest.NewRequest(http.MethodGet, path, nil)
-
-		t := time.Now().Truncate(time.Millisecond)
-
-		payable := models.PayableResource{
-			CompanyNumber: "12345678",
-			Reference:     "abcdef",
-			Links: models.PayableResourceLinks{
-				Self:    "/company/12345678/penalties/late-filing/abcdef",
-				Payment: "/company/12345678/penalties/late-filing/abcdef/payment",
+		testCases := []struct {
+			description           string
+			kind                  string
+			classOfPayment        string
+			descriptionIdentifier string
+			resourceKind          string
+			productType           string
+			companyCode           string
+		}{
+			{
+				description:           "Late Filing Penalty",
+				kind:                  "payment-details#payment-details",
+				classOfPayment:        "penalty",
+				descriptionIdentifier: "late-filing-penalty",
+				resourceKind:          "late-filing-penalty#late-filing-penalty",
+				productType:           "late-filing-penalty",
+				companyCode:           utils.LateFilingPenalty,
 			},
-			Etag:      "qwertyetag1234",
-			CreatedAt: &t,
-			CreatedBy: models.CreatedBy{
-				Email: "test@user.com",
-				ID:    "uz3r1D_H3r3",
-			},
-			Transactions: []models.TransactionItem{
-				{
-					Amount:        5,
-					Type:          "penalty",
-					TransactionID: "A1234567",
-				},
-			},
-			Payment: models.Payment{
-				Amount: "5",
-				Status: "pending",
+			{
+				description:           "Sanctions Penalty Payment",
+				kind:                  "payment-details#payment-details",
+				classOfPayment:        "penalty-sanctions",
+				descriptionIdentifier: "penalty-sanctions",
+				resourceKind:          "penalty#sanctions",
+				productType:           "penalty-sanctions",
+				companyCode:           utils.Sanctions,
 			},
 		}
+		for _, tc := range testCases {
+			Convey(tc.description, func() {
+				path := "/company/12345678/penalties/late-filing/abcdef/payment"
+				req := httptest.NewRequest(http.MethodGet, path, nil)
 
-		service := &PaymentDetailsService{}
+				t := time.Now().Truncate(time.Millisecond)
 
-		paymentDetails, responseType, err := service.GetPaymentDetailsFromPayableResource(req, &payable,
-			penaltyDetailsMap, utils.LateFilingPenalty)
+				payable := models.PayableResource{
+					CompanyNumber: "12345678",
+					Reference:     "abcdef",
+					Links: models.PayableResourceLinks{
+						Self:    "/company/12345678/penalties/late-filing/abcdef",
+						Payment: "/company/12345678/penalties/late-filing/abcdef/payment",
+					},
+					Etag:      "qwertyetag1234",
+					CreatedAt: &t,
+					CreatedBy: models.CreatedBy{
+						Email: "test@user.com",
+						ID:    "uz3r1D_H3r3",
+					},
+					Transactions: []models.TransactionItem{
+						{
+							Amount:        5,
+							Type:          "penalty",
+							TransactionID: "A1234567",
+						},
+					},
+					Payment: models.Payment{
+						Amount: "5",
+						Status: "pending",
+					},
+				}
 
-		expectedCost := models.Cost{
-			Description:             "Late Filing Penalty",
-			Amount:                  "5",
-			AvailablePaymentMethods: []string{"credit-card"},
-			ClassOfPayment:          []string{"penalty"},
-			DescriptionIdentifier:   "late-filing-penalty",
-			Kind:                    "cost#cost",
-			ResourceKind:            "late-filing-penalty#late-filing-penalty",
-			ProductType:             "late-filing-penalty",
+				service := &PaymentDetailsService{}
+
+				paymentDetails, responseType, err := service.GetPaymentDetailsFromPayableResource(req, &payable,
+					penaltyDetailsMap, tc.companyCode)
+
+				expectedCost := models.Cost{
+					Description:             tc.description,
+					Amount:                  "5",
+					AvailablePaymentMethods: []string{"credit-card"},
+					ClassOfPayment:          []string{tc.classOfPayment},
+					DescriptionIdentifier:   tc.descriptionIdentifier,
+					Kind:                    "cost#cost",
+					ResourceKind:            tc.resourceKind,
+					ProductType:             tc.productType,
+				}
+
+				So(paymentDetails, ShouldNotBeNil)
+				So(paymentDetails.Description, ShouldEqual, tc.description)
+				So(paymentDetails.Kind, ShouldEqual, tc.kind)
+				So(paymentDetails.PaymentReference, ShouldEqual, "")
+				So(paymentDetails.Links.Self, ShouldEqual, "/company/12345678/penalties/late-filing/abcdef/payment")
+				So(paymentDetails.Links.Resource, ShouldEqual, "/company/12345678/penalties/late-filing/abcdef")
+				So(paymentDetails.Status, ShouldEqual, "pending")
+				So(paymentDetails.CompanyNumber, ShouldEqual, "12345678")
+				So(paymentDetails.Items[0], ShouldResemble, expectedCost)
+				So(responseType, ShouldEqual, Success)
+				So(err, ShouldBeNil)
+			})
 		}
-
-		So(paymentDetails, ShouldNotBeNil)
-		So(paymentDetails.Description, ShouldEqual, "Late Filing Penalty")
-		So(paymentDetails.Kind, ShouldEqual, "payment-details#payment-details")
-		So(paymentDetails.PaymentReference, ShouldEqual, "")
-		So(paymentDetails.Links.Self, ShouldEqual, "/company/12345678/penalties/late-filing/abcdef/payment")
-		So(paymentDetails.Links.Resource, ShouldEqual, "/company/12345678/penalties/late-filing/abcdef")
-		So(paymentDetails.Status, ShouldEqual, "pending")
-		So(paymentDetails.CompanyNumber, ShouldEqual, "12345678")
-		So(paymentDetails.Items[0], ShouldResemble, expectedCost)
-		So(responseType, ShouldEqual, Success)
-		So(err, ShouldBeNil)
 
 	})
 
 	Convey("Get payment details paid state - success", t, func() {
 
-		path := "/company/12345678/penalties/late-filing/abcdef/payment"
-		req := httptest.NewRequest(http.MethodGet, path, nil)
-
-		t := time.Now().Truncate(time.Millisecond)
-
-		payable := models.PayableResource{
-			CompanyNumber: "12345678",
-			Reference:     "abcdef",
-			Links: models.PayableResourceLinks{
-				Self:    "/company/12345678/penalties/late-filing/abcdef",
-				Payment: "/company/12345678/penalties/late-filing/abcdef/payment",
+		testCases := []struct {
+			description           string
+			kind                  string
+			classOfPayment        string
+			descriptionIdentifier string
+			resourceKind          string
+			productType           string
+			companyCode           string
+		}{
+			{
+				description:           "Late Filing Penalty",
+				kind:                  "payment-details#payment-details",
+				classOfPayment:        "penalty",
+				descriptionIdentifier: "late-filing-penalty",
+				resourceKind:          "late-filing-penalty#late-filing-penalty",
+				productType:           "late-filing-penalty",
+				companyCode:           utils.LateFilingPenalty,
 			},
-			Etag:      "qwertyetag1234",
-			CreatedAt: &t,
-			CreatedBy: models.CreatedBy{
-				Email: "test@user.com",
-				ID:    "uz3r1D_H3r3",
-			},
-			Transactions: []models.TransactionItem{
-				{
-					Amount:        5,
-					Type:          "penalty",
-					TransactionID: "0987654321",
-				},
-			},
-			Payment: models.Payment{
-				Amount:    "50",
-				Status:    "paid",
-				PaidAt:    &t,
-				Reference: "payment_id",
+			{
+				description:           "Sanctions Penalty Payment",
+				kind:                  "payment-details#payment-details",
+				classOfPayment:        "penalty-sanctions",
+				descriptionIdentifier: "penalty-sanctions",
+				resourceKind:          "penalty#sanctions",
+				productType:           "penalty-sanctions",
+				companyCode:           utils.Sanctions,
 			},
 		}
+		for _, tc := range testCases {
+			Convey(tc.description, func() {
+				path := "/company/12345678/penalties/late-filing/abcdef/payment"
+				req := httptest.NewRequest(http.MethodGet, path, nil)
 
-		service := &PaymentDetailsService{}
+				t := time.Now().Truncate(time.Millisecond)
 
-		paymentDetails, responseType, err := service.GetPaymentDetailsFromPayableResource(req, &payable,
-			penaltyDetailsMap, utils.LateFilingPenalty)
+				payable := models.PayableResource{
+					CompanyNumber: "12345678",
+					Reference:     "abcdef",
+					Links: models.PayableResourceLinks{
+						Self:    "/company/12345678/penalties/late-filing/abcdef",
+						Payment: "/company/12345678/penalties/late-filing/abcdef/payment",
+					},
+					Etag:      "qwertyetag1234",
+					CreatedAt: &t,
+					CreatedBy: models.CreatedBy{
+						Email: "test@user.com",
+						ID:    "uz3r1D_H3r3",
+					},
+					Transactions: []models.TransactionItem{
+						{
+							Amount:        5,
+							Type:          "penalty",
+							TransactionID: "0987654321",
+						},
+					},
+					Payment: models.Payment{
+						Amount:    "50",
+						Status:    "paid",
+						PaidAt:    &t,
+						Reference: "payment_id",
+					},
+				}
 
-		expectedCost := models.Cost{
-			Description:             "Late Filing Penalty",
-			Amount:                  "5",
-			AvailablePaymentMethods: []string{"credit-card"},
-			ClassOfPayment:          []string{"penalty"},
-			DescriptionIdentifier:   "late-filing-penalty",
-			Kind:                    "cost#cost",
-			ResourceKind:            "late-filing-penalty#late-filing-penalty",
-			ProductType:             "late-filing-penalty",
+				service := &PaymentDetailsService{}
+
+				paymentDetails, responseType, err := service.GetPaymentDetailsFromPayableResource(req, &payable,
+					penaltyDetailsMap, tc.companyCode)
+
+				expectedCost := models.Cost{
+					Description:             tc.description,
+					Amount:                  "5",
+					AvailablePaymentMethods: []string{"credit-card"},
+					ClassOfPayment:          []string{tc.classOfPayment},
+					DescriptionIdentifier:   tc.descriptionIdentifier,
+					Kind:                    "cost#cost",
+					ResourceKind:            tc.resourceKind,
+					ProductType:             tc.productType,
+				}
+
+				So(paymentDetails, ShouldNotBeNil)
+				So(paymentDetails.Description, ShouldEqual, tc.description)
+				So(paymentDetails.Kind, ShouldEqual, tc.kind)
+				So(paymentDetails.PaidAt, ShouldEqual, &t)
+				So(paymentDetails.PaymentReference, ShouldEqual, "payment_id")
+				So(paymentDetails.Links.Self, ShouldEqual, "/company/12345678/penalties/late-filing/abcdef/payment")
+				So(paymentDetails.Links.Resource, ShouldEqual, "/company/12345678/penalties/late-filing/abcdef")
+				So(paymentDetails.Status, ShouldEqual, "paid")
+				So(paymentDetails.CompanyNumber, ShouldEqual, "12345678")
+				So(paymentDetails.Items[0], ShouldResemble, expectedCost)
+				So(responseType, ShouldEqual, Success)
+				So(err, ShouldBeNil)
+			})
 		}
-
-		So(paymentDetails, ShouldNotBeNil)
-		So(paymentDetails.Description, ShouldEqual, "Late Filing Penalty")
-		So(paymentDetails.Kind, ShouldEqual, "payment-details#payment-details")
-		So(paymentDetails.PaidAt, ShouldEqual, &t)
-		So(paymentDetails.PaymentReference, ShouldEqual, "payment_id")
-		So(paymentDetails.Links.Self, ShouldEqual, "/company/12345678/penalties/late-filing/abcdef/payment")
-		So(paymentDetails.Links.Resource, ShouldEqual, "/company/12345678/penalties/late-filing/abcdef")
-		So(paymentDetails.Status, ShouldEqual, "paid")
-		So(paymentDetails.CompanyNumber, ShouldEqual, "12345678")
-		So(paymentDetails.Items[0], ShouldResemble, expectedCost)
-		So(responseType, ShouldEqual, Success)
-		So(err, ShouldBeNil)
-
-	})
-}
-
-func TestUnitGetPaymentDetailsFromPayableResourceSanctions(t *testing.T) {
-
-	penaltyDetailsMap, err := config.LoadPenaltyDetails("../assets/penalty_details.yml")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	Convey("Get payment details no transactions penalty-sanctions - invalid data", t, func() {
-
-		path := "/company/12345678/penalties/late-filing/abcdef/payment"
-		req := httptest.NewRequest(http.MethodGet, path, nil)
-
-		t := time.Now().Truncate(time.Millisecond)
-
-		payable := models.PayableResource{
-			CompanyNumber: "12345678",
-			Reference:     "abcdef",
-			Links: models.PayableResourceLinks{
-				Self:    "/company/12345678/penalties/late-filing/abcdef",
-				Payment: "/company/12345678/penalties/late-filing/abcdef/payment",
-			},
-			Etag:      "qwertyetag1234",
-			CreatedAt: &t,
-			CreatedBy: models.CreatedBy{
-				Email: "test@user.com",
-				ID:    "uz3r1D_H3r3",
-			},
-			Transactions: []models.TransactionItem{},
-			Payment: models.Payment{
-				Amount: "5",
-				Status: "pending",
-			},
-		}
-
-		service := &PaymentDetailsService{}
-
-		paymentDetails, responseType, err := service.GetPaymentDetailsFromPayableResource(req, &payable,
-			penaltyDetailsMap, utils.Sanctions)
-
-		So(paymentDetails, ShouldBeNil)
-		So(responseType, ShouldEqual, InvalidData)
-		So(err, ShouldNotBeNil)
-
-	})
-
-	Convey("Get payment details pending state penalty-sanctions - success", t, func() {
-
-		path := "/company/12345678/penalties/late-filing/abcdef/payment"
-		req := httptest.NewRequest(http.MethodGet, path, nil)
-
-		t := time.Now().Truncate(time.Millisecond)
-
-		payable := models.PayableResource{
-			CompanyNumber: "12345678",
-			Reference:     "abcdef",
-			Links: models.PayableResourceLinks{
-				Self:    "/company/12345678/penalties/late-filing/abcdef",
-				Payment: "/company/12345678/penalties/late-filing/abcdef/payment",
-			},
-			Etag:      "qwertyetag1234",
-			CreatedAt: &t,
-			CreatedBy: models.CreatedBy{
-				Email: "test@user.com",
-				ID:    "uz3r1D_H3r3",
-			},
-			Transactions: []models.TransactionItem{
-				{
-					Amount:        5,
-					Type:          "penalty",
-					TransactionID: "A1234567",
-				},
-			},
-			Payment: models.Payment{
-				Amount: "5",
-				Status: "pending",
-			},
-		}
-
-		service := &PaymentDetailsService{}
-
-		paymentDetails, responseType, err := service.GetPaymentDetailsFromPayableResource(req, &payable,
-			penaltyDetailsMap, utils.Sanctions)
-
-		expectedCost := models.Cost{
-			Description:             "Sanctions Penalty Payment",
-			Amount:                  "5",
-			AvailablePaymentMethods: []string{"credit-card"},
-			ClassOfPayment:          []string{"penalty-sanctions"},
-			DescriptionIdentifier:   "penalty-sanctions",
-			Kind:                    "cost#cost",
-			ResourceKind:            "penalty#sanctions",
-			ProductType:             "penalty-sanctions",
-		}
-
-		So(paymentDetails, ShouldNotBeNil)
-		So(paymentDetails.Description, ShouldEqual, "Sanctions Penalty Payment")
-		So(paymentDetails.Kind, ShouldEqual, "payment-details#payment-details")
-		So(paymentDetails.PaymentReference, ShouldEqual, "")
-		So(paymentDetails.Links.Self, ShouldEqual, "/company/12345678/penalties/late-filing/abcdef/payment")
-		So(paymentDetails.Links.Resource, ShouldEqual, "/company/12345678/penalties/late-filing/abcdef")
-		So(paymentDetails.Status, ShouldEqual, "pending")
-		So(paymentDetails.CompanyNumber, ShouldEqual, "12345678")
-		So(paymentDetails.Items[0], ShouldResemble, expectedCost)
-		So(responseType, ShouldEqual, Success)
-		So(err, ShouldBeNil)
-
-	})
-
-	Convey("Get payment details paid state penalty-sanctions - success", t, func() {
-
-		path := "/company/12345678/penalties/late-filing/abcdef/payment"
-		req := httptest.NewRequest(http.MethodGet, path, nil)
-
-		t := time.Now().Truncate(time.Millisecond)
-
-		payable := models.PayableResource{
-			CompanyNumber: "12345678",
-			Reference:     "abcdef",
-			Links: models.PayableResourceLinks{
-				Self:    "/company/12345678/penalties/late-filing/abcdef",
-				Payment: "/company/12345678/penalties/late-filing/abcdef/payment",
-			},
-			Etag:      "qwertyetag1234",
-			CreatedAt: &t,
-			CreatedBy: models.CreatedBy{
-				Email: "test@user.com",
-				ID:    "uz3r1D_H3r3",
-			},
-			Transactions: []models.TransactionItem{
-				{
-					Amount:        5,
-					Type:          "penalty",
-					TransactionID: "0987654321",
-				},
-			},
-			Payment: models.Payment{
-				Amount:    "50",
-				Status:    "paid",
-				PaidAt:    &t,
-				Reference: "payment_id",
-			},
-		}
-
-		service := &PaymentDetailsService{}
-
-		paymentDetails, responseType, err := service.GetPaymentDetailsFromPayableResource(req, &payable,
-			penaltyDetailsMap, utils.Sanctions)
-
-		expectedCost := models.Cost{
-			Description:             "Sanctions Penalty Payment",
-			Amount:                  "5",
-			AvailablePaymentMethods: []string{"credit-card"},
-			ClassOfPayment:          []string{"penalty-sanctions"},
-			DescriptionIdentifier:   "penalty-sanctions",
-			Kind:                    "cost#cost",
-			ResourceKind:            "penalty#sanctions",
-			ProductType:             "penalty-sanctions",
-		}
-
-		So(paymentDetails, ShouldNotBeNil)
-		So(paymentDetails.Description, ShouldEqual, "Sanctions Penalty Payment")
-		So(paymentDetails.Kind, ShouldEqual, "payment-details#payment-details")
-		So(paymentDetails.PaidAt, ShouldEqual, &t)
-		So(paymentDetails.PaymentReference, ShouldEqual, "payment_id")
-		So(paymentDetails.Links.Self, ShouldEqual, "/company/12345678/penalties/late-filing/abcdef/payment")
-		So(paymentDetails.Links.Resource, ShouldEqual, "/company/12345678/penalties/late-filing/abcdef")
-		So(paymentDetails.Status, ShouldEqual, "paid")
-		So(paymentDetails.CompanyNumber, ShouldEqual, "12345678")
-		So(paymentDetails.Items[0], ShouldResemble, expectedCost)
-		So(responseType, ShouldEqual, Success)
-		So(err, ShouldBeNil)
-
 	})
 }
