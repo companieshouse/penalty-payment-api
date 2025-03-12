c8858729dda7759baf6dcc716a4c9d62085439bf
dgordon-ch <dgordon@companieshouse.gov.uk>
2025-03-12 10:13:11 +0000

    SAN-338 - Revert class of payment to unblock CS journey

--- transformers/payable_resource_test.go
+++ transformers/payable_resource_test.go
@@ -150,7 +150,7 @@ func TestUnitPayableResourceToPaymentDetails(t *testing.T) {
 				description:           "Sanctions Penalty Payment",
 				kind:                  "payment-details#payment-details",
 				classOfPayment:        "penalty-sanctions",
-				descriptionIdentifier: "penalty-sanctions",
+				descriptionIdentifier: "data-maintenance",
 				resourceKind:          "penalty#sanctions",
 				productType:           "penalty-sanctions",
 				companyCode:           utils.Sanctions,
