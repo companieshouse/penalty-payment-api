e1d54cb16c0212f4a03e635b9ef4b4570ffb4183
dnair-ch <dnair@companieshouse.gov.uk>
2025-05-12 09:05:28 +0100

    SAN-346: ECS Migration

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Start script for file-delivery-api
+# Start script for penalty-payment-api
 PORT="8080"
 if [[ ! -x ./penalty-payment-api ]]; then
   echo "ERROR: ./penalty-payment-api not found or not executable"
