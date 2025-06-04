3d50457838938dc48dc9d26ae9d8f592f5a28f43
dnair-ch <dnair@companieshouse.gov.uk>
2025-06-04 13:26:17 +0100

    SAN-349: To fix Kafka error in staging/live

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -6,4 +6,11 @@ if [[ ! -x ./penalty-payment-api ]]; then
   echo "ERROR: ./penalty-payment-api not found or not executable"
   exit 1
 fi
-exec ./penalty-payment-api "-bind-addr=:${PORT}"
+
+# Read brokers and topics from environment and split on comma
+IFS=',' read -ra BROKERS <<< "${KAFKA_BROKER_ADDR}"
+
+# Ensure we only populate the broker address and topic via application arguments
+unset KAFKA_BROKER_ADDR
+
+exec ./penalty-payment-api "-bind-addr=:${PORT}" $(for broker in "${BROKERS[@]}"; do echo -n "-broker-addr=${broker} "; done)
