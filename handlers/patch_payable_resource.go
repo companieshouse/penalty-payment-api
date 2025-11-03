0d67f2b0028f9430fb1f9f0bedb7809e948e800d
aonubeze-ch <aonubeze@companieshouse.gov.uk>
2025-11-03 10:32:02 +0000

    SAN-754 Fix log data

--- handlers/patch_payable_resource.go
+++ handlers/patch_payable_resource.go
@@ -77,11 +77,11 @@ func PayResourceHandler(payableResourceService *services.PayableResourceService,
 		}
 
 		log.InfoC(requestId, "checking if payment was cancelled",
-			log.Data{"payment_ref": resource.PayableRef, "payable_ref": resource.PayableRef, "payment_status": payment.Status})
+			log.Data{"payment_ref": request.Reference, "payable_ref": resource.PayableRef, "payment_status": payment.Status})
 		if payment.IsCancelled() {
 			log.Event("warn", requestId, log.Data{
 				"message":        "the payment was cancelled",
-				"payment_ref":    resource.PayableRef,
+				"payment_ref":    request.Reference,
 				"payable_ref":    resource.PayableRef,
 				"payment_status": payment.Status,
 			})
