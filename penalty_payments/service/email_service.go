23b7b98170a967ca0e74c513e03de2d13d64ebd7
dtoulcher-ch <dtoulcher@companieshouse.gov.uk>
2026-01-12 16:23:14 +0000

    [KAF-197] add missing endpoint text

--- penalty_payments/service/email_service.go
+++ penalty_payments/service/email_service.go
@@ -34,7 +34,7 @@ func SendEmailMessageViaChsKafkaApi(payableResource models.PayableResource, req 
 		return fmt.Errorf("error preparing email message for chs-kafka-api: [%v]", err)
 	}
 
-	baseURL, err := url.Parse(cfg.ChsKafkaApiURL)
+	baseURL, err := url.Parse(fmt.Sprintf("%s/send-email", cfg.ChsKafkaApiURL))
 	if err != nil {
 		return fmt.Errorf("invalid base URL: [%v]", err)
 	}
@@ -50,7 +50,8 @@ func SendEmailMessageViaChsKafkaApi(payableResource models.PayableResource, req 
 
 	baseURL.RawQuery = params.Encode()
 
-	request, err := newRequestFunc("POST", baseURL.String(), nil)
+	request, err := newRequestFunc(http.MethodPost, baseURL.String(), nil)
+
 	if err != nil {
 		return fmt.Errorf("error creating POST request to chs-kafka-api: [%v]", err)
 	}
